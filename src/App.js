d7ad50143b6403dc2e5a7c8c4c4364f1befc946b
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2023-04-20 09:46:03 +0530

    add form

--- src/App.js
+++ src/App.js
@@ -3,17 +3,47 @@ import Login from './Component/Login';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from './Component/Home';
 import Logout from './Component/Logout';
+import UserContext from './Context/UserContext';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
-  return (
+  const [user,setUser] = useState({name:null,email:null,googleId:null});
+
+  const isLoggedIn = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/isLoggedIn", {}, {
+        withCredentials: true
+      });
+      const content = response.data;
+      console.log(content);
+      if (content.success === "true") {
+        console.log("Content : ", content);
+        console.log("User : ", content.user);
+        // setUser({ id: content.user._id, ...content.user });
+        console.log("User context : ", user);
+      } else {
+        setUser({});  
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(()=>{
+    isLoggedIn();
+  },[])
+   return (
     <>
     <Router>
+      <UserContext.Provider value={{ user, setUser }}>
     <Routes>
     <Route exact path="/" element={<Login/>} />
     <Route exact path="/home" element={<Home/>} />
     <Route exact path="/logout" element={<Logout/>} />
     </Routes>
+    </UserContext.Provider>
     </Router>
     
     </>
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,21 +1,33 @@
-import React from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-// const handleLogin=()=>{
-//   axios.get("http://localhost:4000/auth/google").then(response=>{
-//     console.log(response);
-//   }).catch(error=>{
-//     console.log(error);
-//   }) 
-// }
 
 const Login = () => {
- 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  // const handleForm=(e)=>{
+  //   e.preventDefault();
+  //   const data = axios.get("http://localhost:4000/auth/google").then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // }
   return (
-    <form action="http://localhost:4000/auth/google">
-    <button type="submit"  className="justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign in with Google</button>
-   </form> 
-      );
+    <>
+      <form action="http://localhost:4000/auth/google" >
+       
+        <button
+          type="submit"
+          className="justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Sign in with Google
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
